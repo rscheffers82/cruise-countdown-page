2fab608e876bf59cad128ce7a4a6dcb20df36c15
Roy Scheffers <r.scheffers@gmail.com>
2017-03-21 22:55:24 -0400

    Update overall styles

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -13,24 +13,24 @@ class Countdown extends Component {
     return (
       <div className="time-wrapper">
         <span className="days">
-          <div>{lang.days.en}</div>
           <div className="part">1</div>
           <div className="part two">0</div>
+          <div>{lang.days.en}</div>
         </span>
         <span className="hours">
-          <div>{lang.hours.en}</div>
           <div className="part">1</div>
           <div className="part two">2</div>
+          <div>{lang.hours.en}</div>
         </span>
         <span className="minutes">
-          <div>{lang.minutes.en}</div>
           <div className="part">0</div>
           <div className="part two">9</div>
+          <div>{lang.minutes.en}</div>
         </span>
         <span className="seconds">
-          <div>{lang.seconds.en}</div>
           <div className="part">2</div>
           <div className="part two">3</div>
+          <div>{lang.seconds.en}</div>
         </span>
       </div>
     );
