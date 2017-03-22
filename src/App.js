256b35e3b3cb68450d995b15db37c958dd9b3ed6
Roy Scheffers <r.scheffers@gmail.com>
2017-03-21 22:00:54 -0400

    Add initial structure for DisplayTime

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 // import logo from './images/logo.svg';
+import DisplayTime from './components/DisplayTime';
+import lang from './languages/languages.json';
 
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      lang: 'en'
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -12,15 +20,20 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div> */}
         <p className="App-intro">
+          <h1>{lang.timerHeading.en}</h1>
           <div className="cruise"></div>
         </p>
         <div className="flags-container">
           <div className="flag english"></div>
           <div className="flag german"></div>
         </div>
+        <DisplayTime />
       </div>
     );
   }
 }
 
 export default App;
+
+// boilerplate details - https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-a-stylesheet
+// cruise - https://www.carnival.com/cruise-ships/carnival-splendor.aspx
---
+++ src/components/DisplayTime.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+// import moment from 'moment';
+import lang from './../languages/languages.json';
+
+import './DisplayTime.css';
+
+class Time extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+
+    return (
+      <div className="time-wrapper">
+        <span className="days">
+          <div>{lang.days.en}</div>
+          <div className="part">1</div>
+          <div className="part two">0</div>
+        </span>
+        <span className="hours">
+          <div>{lang.hours.en}</div>
+          <div className="part">1</div>
+          <div className="part two">2</div>
+        </span>
+        <span className="minutes">
+          <div>{lang.minutes.en}</div>
+          <div className="part">0</div>
+          <div className="part two">9</div>
+        </span>
+        <span className="seconds">
+          <div>{lang.seconds.en}</div>
+          <div className="part">2</div>
+          <div className="part two">3</div>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Time;
